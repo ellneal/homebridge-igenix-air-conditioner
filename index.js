01234ed408d17ebea6fe3aef853fa8bb362645aa
Ell Neal <elliot@emdentec.com>
2018-07-12 23:15:41 +0100

    Implemend airbnb style guide

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = (homebridge) => {
+    // eslint-disable-next-line global-require
     const IgenixAirConditioner = require('./lib/IgenixAirConditioner')(homebridge);
     homebridge.registerAccessory('homebridge-igenix-air-conditioner', 'IgenixAirConditioner', IgenixAirConditioner);
 };
