ab34273c19c7e01ebfee9e840beb3df936f042b8
Ell Neal <elliot@emdentec.com>
2018-07-12 17:33:19 +0100

    Remove unnecessary import

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const debug = require('debug')('IgenixAirConditioner');
-
 module.exports = (homebridge) => {
     const IgenixAirConditioner = require('./lib/IgenixAirConditioner')(homebridge);
     homebridge.registerAccessory('homebridge-igenix-air-conditioner', 'IgenixAirConditioner', IgenixAirConditioner);
